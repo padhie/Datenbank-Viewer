3b3018342d214b3c66eb56830a282afb02fc61a5
padhie <padhiez@hotmail.de>
2016-06-29 22:54:35 +0200

    no message

--- pma/mainconfig.php
+++ pma/mainconfig.php
@@ -1,15 +1,14 @@
 <?php
 
-$aPmaList = array(
-	"--- Version auswählen ---"
-);
-foreach (array_reverse(getListOfDir("pma/")) AS $sEntry) {
+$aPmaList = array();
+foreach (array_reverse(getListOfDir(__DIR__)) AS $sEntry) {
 	if (!strpos($sEntry, ".php") && !strpos($sEntry, ".html") && !strpos($sEntry, ".css")) {
 		$sGroup = substr($sEntry, 0, strpos($sEntry, "_"));
 		if (!isset($aPmaList[$sGroup])) {
 			$aPmaList[$sGroup] = array();
 		}
-		array_push($aPmaList[$sGroup], $sEntry);
+		$sDisplayName = str_replace("_", ".", $sEntry);
+		$aPmaList[$sGroup][$sDisplayName] = "/pma/".$sEntry."/";
 	}
 }
 
