9078d1f67ced3ed7385b11d2a5a40f9b5cfe43cd
padhie <padhiez@hotmail.de>
2016-06-29 20:49:41 +0200

    no message

--- sqlbuddy/mainconfig.php
+++ sqlbuddy/mainconfig.php
@@ -1,8 +1,16 @@
 <?php
 
-return array(
-	"--- Version auswählen ---",
-	"1.x" => array(
-		"1.3.3"
-	)
-);
+$aList = array(
+     "--- Version auswählen ---"
+ );
+foreach (array_reverse(getListOfDir(__DIR__)) AS $sEntry) {
+     if (!strpos($sEntry, ".php") && !strpos($sEntry, ".html") && !strpos($sEntry, ".css")) {
+         $sGroup = substr($sEntry, 0, strpos($sEntry, "_"));
+         if (!isset($aList[$sGroup])) {
+             $aList[$sGroup] = array();
+         }
+         array_push($aList[$sGroup], $sEntry);
+     }
+}
+
+return $aList;
