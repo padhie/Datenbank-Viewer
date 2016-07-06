514caa0c3fb54b9516c4e7a3d449ba851ac3ee73
padhie <padhiez@hotmail.de>
2016-07-06 20:16:48 +0200

    liste erstellt

--- phpminiadmin/mainconfig.php
+++ phpminiadmin/mainconfig.php
@@ -1,16 +1,16 @@
 <?php
 
-$aList = array(
-     "--- Version auswählen ---"
- );
+$aList = array();
+
 foreach (array_reverse(getListOfDir(__DIR__)) AS $sEntry) {
-     if (!strpos($sEntry, ".php") && !strpos($sEntry, ".html") && !strpos($sEntry, ".css")) {
-         $sGroup = substr($sEntry, 0, strpos($sEntry, "_"));
-         if (!isset($aList[$sGroup])) {
-             $aList[$sGroup] = array();
-         }
-         array_push($aList[$sGroup], $sEntry);
-     }
+	if (!strpos($sEntry, ".php") && !strpos($sEntry, ".html") && !strpos($sEntry, ".css")) {
+		$sDisplayName = "Version ".substr($sEntry, 0, strpos($sEntry, "_")).".x";
+		if (!isset($aList[$sDisplayName])) {
+			$aList[$sDisplayName] = array();
+		}
+		$sVersion = str_replace("_", ".", $sEntry);
+		$aList[$sDisplayName][$sVersion]	= "/phpminiadmin/".$sEntry."/phpminiadmin.php";
+	}
 }
 
 return $aList;
