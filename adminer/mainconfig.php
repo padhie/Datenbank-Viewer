4e45f41fc56dd352f22a3b882a45efcf9f83e268
padhie <padhiez@hotmail.de>
2016-07-06 19:06:35 +0200

    Default layout

--- adminer/mainconfig.php
+++ adminer/mainconfig.php
@@ -4,6 +4,8 @@ $aAdminerList = array();
 foreach (array_reverse(getListOfDir(__DIR__)) AS $sVersion) {
 	if (!strpos($sVersion, ".php") && !strpos($sVersion, ".html") && !strpos($sVersion, ".css")) {
 		$sDisplayVersion = "Version ".str_replace("_", ".", $sVersion);
+		$aAdminerList[$sDisplayVersion]["default"] = "/adminer/index.php?version=".$sVersion;
+		
 		if (!isset($aAdminerList[$sDisplayVersion])) {
 			$aAdminerList[$sDisplayVersion] = array();
 		}
